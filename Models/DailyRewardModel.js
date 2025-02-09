be6f1758331498321cd4cc8b2785afa5b2239619
Coder Bhai <yadspakistan.pk.@gmail.com>
2025-02-09 17:58:38 +0500

    Update DailyRewardModel.js

--- Models/DailyRewardModel.js
+++ Models/DailyRewardModel.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const dailyRewardSchema = new mongoose.Schema({
   prize: { type: String, required: true }, // Reward type (e.g., Coins, Bonus)
-  amount: { type: Number, required: true }, // Amount of reward
-  day: { type: Number, required: true, unique: true }, // Day number (e.g., 1,2,3,...)
+  amount: { type: String, required: true }, // Amount of reward
+  day: { type: String, required: true, unique: true }, // Day number (e.g., 1,2,3,...)
   isActive: { type: Boolean, default: true }, // Active or inactive
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
